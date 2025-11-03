18ef611b0dc18e67a5feec7b25d4997ba94befa6
ItsElex <142209057+ItsElex@users.noreply.github.com>
2025-11-03 15:54:59 +0100

    fixed typo

--- src/rov_black_box/launch/video.launch.py
+++ src/rov_black_box/launch/video.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
 
-def generate_leaunch_description():
+def generate_launch_description():
     namespace = LaunchConfiguration('namespace')
 
     return LaunchDescription([
